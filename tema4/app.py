61209ca2766a40572b3770026382a61f497d6de8
Mocanu Octavian <mocanuoctavian12@gmail.com>
2024-04-29 11:51:13 +0300

    Added Immersive Reader

--- tema4/app.py
+++ tema4/app.py
@@ -1,5 +1,14 @@
 import os
+import traceback
+import os
+import requests
+import json
+from dotenv import load_dotenv
+from flask import Flask, redirect, render_template, request, jsonify
 
+app = Flask(__name__)
+
+load_dotenv()
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
@@ -27,6 +36,50 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+
+
+
+
+@app.route('/reader')
+def reader():
+    return render_template('reader.html')
+
+
+@app.route('/options')
+def options():
+    'Show the options page'
+    return render_template('options.html')
+
+
+@app.route('/GetTokenAndSubdomain', methods=['GET'])
+def getTokenAndSubdomain():
+    'Get the access token'
+    if request.method == 'GET':
+        try:
+            headers = {'content-type': 'application/x-www-form-urlencoded'}
+            data = {
+                'client_id': str(os.environ.get('CLIENT_ID')),
+                'client_secret': str(os.environ.get('CLIENT_SECRET')),
+                'resource': 'https://cognitiveservices.azure.com/',
+                'grant_type': 'client_credentials'
+            }
+
+            resp = requests.post('https://login.windows.net/' + str(os.environ.get('TENANT_ID')) + '/oauth2/token',
+                                 data=data, headers=headers)
+            jsonResp = resp.json()
+
+            if ('access_token' not in jsonResp):
+                print(jsonResp)
+                raise Exception('AAD Authentication error')
+
+            token = jsonResp['access_token']
+            subdomain = str(os.environ.get('SUBDOMAIN'))
+
+            return jsonify(token=token, subdomain=subdomain)
+        except Exception as e:
+            message = 'Unable to acquire Azure AD token. Check the debugger for more information.'
+            print(message, e)
+            return jsonify(error=message)
 
 if __name__ == '__main__':
    app.run()
